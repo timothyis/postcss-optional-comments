69224bfa658fb48509491d15cadd22979d3234ab
CodeTheory <hello@timothy.is>
2016-01-08 17:27:09 +0100

    Test inline optional comments

--- index.js
+++ index.js
@@ -3,5 +3,11 @@
 var postcss = require('postcss');
 
 module.exports = postcss.plugin('postcss-optional-comments', function(opts) {
-  
+  return function(root) {
+    root.walkDecls(function(decl) {
+      if ( decl.prop.match(/^\/\/![\s]?.+/) ) {
+        decl.remove();
+      }
+    });
+  };
 });
